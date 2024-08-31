4bd81e30aea65d9599322e6cb490bb98a98ac64c
Guada <guadapdls@gmail.com>
2024-08-30 21:27:42 -0300

    contact

--- src/Routes/Contact/Contact.jsx
+++ src/Routes/Contact/Contact.jsx
@@ -1,6 +1,18 @@
+import './Contact.css'
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section>
+      <form id="form" data-aos="zoom-in-up" data-aos-duration="1000">
+        <h4>Formulario de contacto</h4>
+        <input type="text" placeholder="Nombre" name="name" id="nam" required />
+        <input type="text" placeholder="Apellido" name="last" id="last" required />
+        <input type="email" placeholder="Correo electrónico" name="mail" id="mail" required />
+        <textarea cols="30" rows="5" placeholder="Asunto" name="msg" id="msg" required></textarea>
+        <input type="submit" id="button" value="Enviar" className="button" />
+        <div className="resultado"></div>
+      </form>
+    </section>
   )
 }
 
