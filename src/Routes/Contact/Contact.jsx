ce455478c76b1870efbcf9f19650af053df725c9
Guada <guadapdls@gmail.com>
2024-09-09 22:37:42 -0300

    credentials

--- src/Routes/Contact/Contact.jsx
+++ src/Routes/Contact/Contact.jsx
@@ -76,6 +76,7 @@ const Contact = () => {
             mail: formData.mail,
             msg: formData.msg
           }),
+          credentials: 'include'
         });
 
         if (!response.ok) {
