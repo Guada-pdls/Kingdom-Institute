f06bfb7a04df38b9f2f0a5b1c63203c7ebd0b819
Guada <guadapdls@gmail.com>
2025-02-07 01:08:06 -0300

    courses, exams and gallery styles

--- src/Components/CourseSection/CourseSection.jsx
+++ src/Components/CourseSection/CourseSection.jsx
@@ -2,7 +2,7 @@
 const CourseSection = ({ title, description, details }) => {
   return (
     <div className="pb-4">
-      <h4 data-aos="fade-right" className="my-4">{title}</h4>
+      <h4 data-aos="fade-right" className="mb-4">{title}</h4>
       <p className="py-0 mb-0"><span className="sangria">{description}</span></p>
       <p className="py-0 mb-0"><span className="sangria">{details}</span></p>
     </div>
