59dc2b24f22cae50f94b2197c03d93112b479297
Guada <guadapdls@gmail.com>
2025-02-09 00:43:45 -0300

    adding clgs to solve contact form problem

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -3,6 +3,7 @@ import process from 'node:process';
 
 export default async function handler(req, res) {
   console.log('req.method', req.method);
+  console.log(req.body);
   if (req.method !== 'POST') {
     return res.status(405).send({ message: 'Only POST requests are allowed' });
   }
