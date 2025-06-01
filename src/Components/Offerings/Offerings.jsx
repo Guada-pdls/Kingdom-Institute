04f21f39552c145d16bc92463c4baafaf0acc9e0
Guada <guadapdls@gmail.com>
2025-05-31 22:55:21 -0300

    delete gallery, text corrections in offerings and courses

--- src/Components/Offerings/Offerings.jsx
+++ src/Components/Offerings/Offerings.jsx
@@ -5,13 +5,13 @@ const Offerings = () => {
       <div className="offerings" data-aos="zoom-in">
         <h3>OFRECEMOS</h3>
         <ul>
-          <li><img src="./icons/arrow-right.svg" alt="Flecha" />Cursos para todas las edades.</li>
+          <li><img src="./icons/arrow-right.svg" alt="Flecha" />Cursos para todas las edades en modalidad presencial o virtual.</li>
           <li><img src="./icons/arrow-right.svg" alt="Flecha" />Exámenes con aval internacional de las universidades de Cambridge, St. Clare&apos;s y Michigan.</li>
           <li><img src="./icons/arrow-right.svg" alt="Flecha" />Nuestros exámenes AT5, AT6 o Audeppi Pre First te permiten <span>exonerar Inglés en 5to y 6to de bachillerato</span>.</li>
           <li><img src="./icons/arrow-right.svg" alt="Flecha" />Salidas didácticas</li>
           <li><img src="./icons/arrow-right.svg" alt="Flecha" />Bonificaciones especiales para familiares o amigos</li>
           <li><img src="./icons/arrow-right.svg" alt="Flecha" />Clases de apoyo para escolares y liceales</li>
-          <li><img src="./icons/arrow-right.svg" alt="Flecha" />Horarios flexibles</li>
+          <li><img src="./icons/arrow-right.svg" alt="Flecha" />Cursos en la mañana y en la tarde</li>
         </ul>
       </div>
   );
